76c433a99eae7d0e25d5029cda6455eb53d1b727
Aymanjeb <aymanjeb02@gmail.com>
2024-01-09 14:56:40 +0100

    old version

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request, jsonify
-import requests 
 
 app = Flask(__name__)
 
@@ -8,16 +7,8 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/search', methods=['POST'])
-def search():
-    company_name = request.form['company_name']
-    url = f"https://entreprise.data.gouv.fr/api/sirene/v1/full_text/{company_name}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return jsonify(response.json())
-    else:
-        return jsonify({"error": "Data not found"}), 404
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.envir
+               on.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
